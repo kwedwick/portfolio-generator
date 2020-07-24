4aa0b72f9b790738782dc55fb96f130a8ed8ef19
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-07-24 14:24:18 -0500

    refractored code and removed callback functions within cb functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const generateSite = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2);
 
@@ -137,20 +137,45 @@ Add a New Project
     });
 };
 
+
 // promptUser()
-//   .then(answers => console.log(answers))
 //   .then(promptProject)
-//   .then(projectAnswers => console.log(projectAnswers));
+//   .then(portfolioData => {
+//     //const pageHTML = generatePage(mockData);
+//     const pageHTML = generatePage(portfolioData);
+
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Page created! Check out index.html in this directory to see it!');
+
+//       fs.copyFile('./src/style.css', './dist/style.css', err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Style sheet copied successfully!');
+//       });
+//     });
+//   });
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    //const pageHTML = generatePage(mockData);
-    const pageHTML = generatePage(portfolioData);
-
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-
-    //   console.log('Portfolio complete! Check out index.html to see the output!');
-    // });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
